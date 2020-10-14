7108ed9e34c1c92ff80f1286ae4c281384e70fc2
Vando dos reis <vandodosreis2001@gmail.com>
2020-10-14 20:44:06 -0300

    3 page criada e pronta

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,49 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheel: false,
+  zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-23.5551084,-46.3928731], 15)
+
+// create and add titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+// create and add marker
+L.marker([-23.5551084,-46.3928731], { icon })
+.addTo(map)
+
+// image gallery
+
+function selectImage(event) {
+  const button = event.currentTarget
+
+  // remover todas as classes .active
+  const buttons = document.querySelectorAll('.images button')
+  // Para cada botao execute o button
+  buttons.forEach( button => {
+    // button do escopo da função
+    button.classList.remove('active')
+  })
+  // selecionar a image clicada
+  const image = button.children[0]
+  const imageContainer = document.querySelector('.orphanage-details > img')
+
+  // atualizar o container de image
+  imageContainer.src = image.src
+  
+  // adicionar a classe .active para este botao
+  button.classList.add('active')
+
+}
