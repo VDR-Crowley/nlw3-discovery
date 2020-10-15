e4ce53850586fa5bd6e11343f9e42f463c058cd2
Vando dos reis <vandodosreis2001@gmail.com>
2020-10-15 18:06:41 -0300

    Melhorando a organização do codigo

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -2,7 +2,7 @@
 const map = L.map('mapid').setView([-23.5551084,-46.3928731], 15)
 
 // create and add titleLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map)
 
 // create icon
 const icon = L.icon({
@@ -18,7 +18,8 @@ const popup = L.popup({
   className: 'map-popup',
   minWidth: 240,
   minHeight: 240
-}).setContent('Lar das meninas <a href="orphanages.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"/> </a>')
+  // faz os botao ir para a page orphanage
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"/> </a>')
 
 // create and add marker
 L.marker([-23.5551084,-46.3928731], { icon })
